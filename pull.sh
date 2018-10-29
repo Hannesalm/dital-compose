c27909e3de17968ab4f4045f05620d1639393df4
Hannes Alm <hannes@almar.se>
2018-10-29 21:44:50 +0100

    Updated bash script

--- pull.sh
+++ pull.sh
@@ -1,6 +1,9 @@
  #!/bin/bash 
 clear
-   
+
+echo "$(tput setaf 1)Stopping running containers$(tput sgr0)"
+docker-compose down
+
 echo "$(tput setaf 1)Pulling dss-app from docker hub$(tput sgr0)"
 
 docker pull qloud77/dss
