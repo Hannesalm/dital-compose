a81dfa04d7b063e386126cbce864e2b350cb962a
Hannes Alm <hannes@almar.se>
2018-10-29 21:46:29 +0100

    Updated bash script

--- pull.sh
+++ pull.sh
@@ -11,3 +11,7 @@ docker pull qloud77/dss
 echo "$(tput setaf 1)Pulling dss-backendfrom docker hub$(tput sgr0)"
 
 docker pull qloud77/lumen
+
+echo "$(tput setaf 1)Rerunn containers$(tput sgr0)"
+
+docker-compose up -d
