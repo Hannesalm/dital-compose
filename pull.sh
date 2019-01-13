ed277d8850bf86dae95124aeadd73e09172196e6
Hannes Alm <hannes@almar.se>
2019-01-13 22:58:59 +0100

    updated script file

--- pull.sh
+++ pull.sh
@@ -2,16 +2,16 @@
 clear
 
 echo "$(tput setaf 1)Stopping running containers$(tput sgr0)"
-docker-compose down
+sudo docker-compose down
 
 echo "$(tput setaf 1)Pulling app from docker hub$(tput sgr0)"
 
-docker pull qloud77/dital
+sudo docker pull qloud77/dital
 
 echo "$(tput setaf 1)Start db containers$(tput sgr0)"
 
-docker-compose -f db.yml up -d
+sudo docker-compose -f db.yml up -d
 
 echo "$(tput setaf 1)Start Laravel containers$(tput sgr0)"
 
-docker-compose -f offices.yml up -d
+sudo docker-compose -f offices.yml up -d
